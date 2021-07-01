fddf3ab118be3e0da1f75bbc0685744beb8e1eeb
Nha <18130158@st.hcmuaf.edu.vn>
2021-07-01 13:00:47 +0700

    1:00pm 1/7/2021

--- src/main/java/com/cdweb/controller/admin/AdminController.java
+++ src/main/java/com/cdweb/controller/admin/AdminController.java
@@ -15,6 +15,7 @@ import com.cdweb.service.IOrderedService;
 import com.cdweb.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -50,35 +51,39 @@ public class AdminController {
     }
 
     @GetMapping("/admin-productAdd")
-    public ModelAndView addProductPage() {
+    public ModelAndView addProductPage(Principal principal) {
         ModelAndView mav = new ModelAndView("admin/product-add.html");
         mav.addObject("AddBookInput", new AddBookInput());
         mav.addObject("listcategory", categoryService.findAll());
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
     @PostMapping("/admin-productAdd")
-    public String addProduct(@ModelAttribute("AddBookInput") AddBookInput bookInput) {
+    public String addProduct(@ModelAttribute("AddBookInput") AddBookInput bookInput, Principal principal) {
         ModelAndView mav = new ModelAndView("admin/product-add.html");
         mav.addObject("AddBookInput", new AddBookInput());
         mav.addObject("listcategory", categoryService.findAll());
+        mav.addObject("username", principal.getName());
         return "redirect:/admin";
     }
 
     @PostMapping("/admin-productEdit")
-    public String editProduct(@ModelAttribute("AddBookInput") AddBookInput bookInput) {
+    public String editProduct(@ModelAttribute("AddBookInput") AddBookInput bookInput, Principal principal) {
         ModelAndView mav = new ModelAndView("admin/product-add.html");
         mav.addObject("AddBookInput", new AddBookInput());
         mav.addObject("listcategory", categoryService.findAll());
+        mav.addObject("username", principal.getName());
         return "redirect:/admin";
     }
 
     @GetMapping("/admin-productEdit")
-    public ModelAndView editProductPage(@Param("id") long id) {
+    public ModelAndView editProductPage(@Param("id") long id, Principal principal) {
         ModelAndView mav = new ModelAndView("admin/product-add.html");
         mav.addObject("AddBookInput", new AddBookInput());
         mav.addObject("listcategory", categoryService.findAll());
         mav.addObject("product", bookService.findById(id));
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
@@ -87,24 +92,27 @@ public class AdminController {
     //    CATEGORY    //
 
     @GetMapping("/admin-listCategory")
-    public ModelAndView listCategory() {
+    public ModelAndView listCategory(Principal principal) {
         ModelAndView mav = new ModelAndView("admin/productType.html");
         mav.addObject("listcategory", categoryService.findAll());
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
     @GetMapping("/admin-addType")
-    public ModelAndView addCategory() {
+    public ModelAndView addCategory(Principal principal) {
         ModelAndView mav = new ModelAndView("admin/productType-add.html");
         mav.addObject("category", new CategoryDTO());
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
     @RequestMapping(value = "/admin-addType", method = RequestMethod.POST)
-    public ModelAndView addType(@ModelAttribute("category") CategoryDTO category) {
+    public ModelAndView addType(@ModelAttribute("category") CategoryDTO category, Principal principal) {
         categoryService.save(category);
         ModelAndView mav = new ModelAndView("admin/productType.html");
         mav.addObject("listcategory", categoryService.findAll());
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
@@ -113,16 +121,18 @@ public class AdminController {
     //    ORDER  //
 
     @GetMapping("/admin-listOrder")
-    public ModelAndView listOrder() {
+    public ModelAndView listOrder(Principal principal) {
         ModelAndView mav = new ModelAndView("admin/cart");
         mav.addObject("listOrders", orderedService.findAll());
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
     @GetMapping("/admin-orderDetail")
-    public ModelAndView orderDetail(@PathVariable("id") long id) {
+    public ModelAndView orderDetail(@PathVariable("id") long id, Principal principal) {
         ModelAndView mav = new ModelAndView("admin/cart-detail.html");
         mav.addObject("order", orderedService.findOrder(id));
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
@@ -133,19 +143,21 @@ public class AdminController {
     public ModelAndView listCustomer(Principal principal) {
         ModelAndView mav = new ModelAndView("admin/customer.html");
         mav.addObject("listAllUser", userService.findAll());
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
     @GetMapping(value = "/admin-addUser")
-    public ModelAndView getAddUserPage() {
+    public ModelAndView getAddUserPage(Principal principal) {
         ModelAndView mav = new ModelAndView("admin/customer-add.html");
         mav.addObject("userinput", new UserInput());
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
 
     @RequestMapping(value = "/admin-addUser", method = RequestMethod.POST)
-    public ModelAndView addUser(@ModelAttribute("userinput") UserInput userinput) {
+    public ModelAndView addUser(@ModelAttribute("userinput") UserInput userinput, Principal principal) {
         UserDTO user = new UserDTO();
         List<RoleDTO> roleDTO = new ArrayList<>();
         for (String s : userinput.getRole()) {
@@ -163,19 +175,21 @@ public class AdminController {
         userService.save(user);
         ModelAndView mav = new ModelAndView("admin/customer.html");
         mav.addObject("listAllUser", userService.findAll());
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
     @GetMapping(value = "/admin-editUser")
-    public ModelAndView editUserPage(@Param("email") String email) {
+    public ModelAndView editUserPage(@Param("email") String email, Principal principal) {
         ModelAndView mav = new ModelAndView("admin/customer-edit.html");
         mav.addObject("user", userService.findEmail(email));
         mav.addObject("useremail", email);
+        mav.addObject("username", principal.getName());
         return mav;
     }
 
     @GetMapping(value = "/admin-deleteUser")
-    public ModelAndView deleteUserPage(@Param("email") String email) {
+    public ModelAndView deleteUserPage(@Param("email") String email, Principal principal) {
 
         this.userService.delete(email);
 
@@ -185,7 +199,7 @@ public class AdminController {
     }
 
     @RequestMapping(value = "/admin-editUser", method = RequestMethod.POST)
-    public ModelAndView editUser(@ModelAttribute("userinput") UserInput userinput) {
+    public ModelAndView editUser(@ModelAttribute("userinput") UserInput userinput, Principal principal) {
         UserDTO user = new UserDTO();
         List<RoleDTO> roleDTO = new ArrayList<>();
         for (String s : userinput.getRole()) {
@@ -204,6 +218,7 @@ public class AdminController {
         this.userService.changePassword(user);
         ModelAndView mav = new ModelAndView("admin/customer.html");
         mav.addObject("listAllUser", userService.findAll());
+        mav.addObject("username", principal.getName());
         return mav;
     }
 }
